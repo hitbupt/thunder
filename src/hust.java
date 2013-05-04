6bec2493ff46259343c8606baed09e1ea220f6c7
byxiangfei <zhangxiangBUPT@163.com>
2013-05-04 20:37:03 +0800

    I WANT TO TEST A LITTLE BIT

---
+++ src/hust.java
@@ -0,0 +1,26 @@
+
+
+import java.util.*;
+
+public class hust {
+	public static void main(String args[]) {
+		Scanner cin = new Scanner(System.in);
+		int T = cin.nextInt();
+		
+		while(T--!=0) {
+			int A = cin.nextInt();
+                        int tmp = 1;
+                        int time = A;
+			while(time--!=0)
+			{
+                                 if(tmp<10)
+                                    tmp = tmp*A;
+
+                                 else
+                                    tmp = (tmp%10)*A;
+			}
+		
+			System.out.println(tmp%10);
+               }
+	}
+}
